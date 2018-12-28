dcd706998f3d2e4b0256f4554b88fdcc68c44756
Scott Bellware <sbellware@gmail.com>
2018-12-28 15:39:49 -0600

    Tests are run with the TestBench runner rather than CLI

--- test/automated.rb
+++ test/automated.rb
@@ -1,8 +1,6 @@
-ENV['TEST_BENCH_EXCLUDE_PATTERN'] ||= '/_|sketch|(_init\.rb|_tests\.rb)\z'
-ENV['TEST_BENCH_TESTS_DIR'] ||= 'test/automated'
+require_relative 'test_init'
 
-require_relative './test_init'
-
-require 'test_bench/cli'
-
-TestBench::CLI.() or exit 1
+TestBench::Runner.(
+  'automated/**/*.rb',
+  exclude_pattern: %r{\/_|(_init\.rb|_tests\.rb)\z}
+) or exit 1
